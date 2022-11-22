f6d3fba006409e87ada344055de55bc0b454750e
austin bennett <awb157@gmail.com>
2022-11-22 10:54:15 -0700

    testing engineer class in work status info

---
+++ index.js
@@ -0,0 +1,9 @@
+const Engineer=require("./lib/Engineer");
+const Intern=require("./lib/Intern");
+const Manager=require("./lib/Manager");
+const inquirer=require("inquirer");
+const fs= require("fs");
+
+
+const worker=new Engineer("austin",20395,"awb157@gmial.com","awb157")
+console.log(worker)
